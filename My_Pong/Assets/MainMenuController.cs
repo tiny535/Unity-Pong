e7c07208c39d9c5c40aa0003f47d2f999a0a8570
tiny535 <106269347+tiny535@users.noreply.github.com>
2023-03-06 17:54:02 +1100

    Added a functional pause menu

--- My_Pong/Assets/MainMenuController.cs
+++ My_Pong/Assets/MainMenuController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public void PlayGame() {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadSceneAsync("MainGame");
     }
 
     public void QuitGame() {
